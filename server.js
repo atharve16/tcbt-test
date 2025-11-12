0d8e5982acaa114a6fff3759df91932f6d2b85bc
Atharve Agrawal <atharve211@gmail.com>
2025-11-12 13:40:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.post("/api/data", (req, res) => {
+  const clientData = req.body;
+  console.log("Data received from client:", clientData);
+
+  res.status(200).json({
+    message: "Data received successfully!",
+    receivedData: clientData,
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
