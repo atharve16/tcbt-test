4f270bd1a7f08f7355b5a40993f602f7e09af82c
Atharve Agrawal <atharve211@gmail.com>
2025-11-12 14:57:41 +0530

    production

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 
+const app = express();
+
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+  origin: ["https://www.tcbtjaivikkisan.com", "http://localhost:3000"],
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
@@ -12,14 +22,17 @@ app.get("/", (req, res) => {
 app.post("/api/data", (req, res) => {
   const clientData = req.body;
   console.log("Data received from client:", clientData);
-
   res.status(200).json({
     message: "Data received successfully!",
     receivedData: clientData,
   });
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
-});
+if (process.env.RENDER) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`✅ Server running on http://localhost:${PORT}`);
+  });
+} else {
+  module.exports = app;
+}
